263f5ec1cef4523e2b2e8add37982a58b6a18fc1
lulin-code <linlinlu77@163.com>
2021-10-28 10:58:46 +0800

    组件自定义事件

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,17 @@
  * @Author: 卢林
  * @Date: 2021-10-25 14:23:52
  * @LastEditors: 卢林
- * @LastEditTime: 2021-10-28 10:15:15
+ * @LastEditTime: 2021-10-28 10:58:17
  * @Descripttion: 自定义组件
 -->
 <template>
 	<div>
     <School :getSchoolName='getSchoolName' ></School><br>
     <!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据（使用@或者v-on） -->
-    <!-- <Student @ll='getStudent'></Student> -->
+    <!-- <Student @ll='getStudent' @demo='m1'></Student> -->
 
     <!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据（第二种写法，使用ref） -->
-    <Student ref="student"></Student>
+    <Student ref="student" @click.native="show"></Student>
 
 	</div>
 </template>
@@ -36,10 +36,16 @@
       },
       getStudent(name,...prams){
         console.log('APP收到了学生名称：',name,prams)
+      },
+      m1(){
+        console.log('事件被触发')
+      },
+      show(){
+        alert(123)
       }
     },
     mounted() {
-      this.$refs.student.$on('ll',this.getStudent)
+      // this.$refs.student.$on('ll',this.getStudent)
     },
 	
 	}
--- src/components/Student.vue
+++ src/components/Student.vue
@@ -2,7 +2,7 @@
  * @Author: 卢林
  * @Date: 2021-08-11 08:47:54
  * @LastEditors: 卢林
- * @LastEditTime: 2021-10-28 10:14:47
+ * @LastEditTime: 2021-10-28 10:37:19
  * @Descripttion: 文件描述
 -->
 <template>
@@ -10,6 +10,8 @@
 		<h2>学生名称：{{name}}</h2>
 		<h2 class="atg">学生性别：{{sex}}</h2>
     <button @click="sendStudentName">发送学生姓名</button>
+    <button @click="unbind">解绑ll事件</button>
+    <button @click="death">销毁当前Student组件的实例（VC）</button>
 	</div>
 </template>
 
@@ -25,6 +27,21 @@
     methods: {
       sendStudentName(){
         this.$emit('ll',this.name,666,888,900)
+        this.$emit('demo')
+      },
+      unbind(){
+        // 解绑一个事件
+        // this.$off('ll')  
+
+        // 解绑多个事件
+        this.$off(['ll','demo'])
+
+        //解绑所有的自定义事件
+        // this.$off()
+      },
+      death(){
+        //销毁了当前Student组件的实例，销毁后所有Student实例的自定义事件全都不奏效。
+        this.$destroy()
       }
     },
 	}
