c0df0b34e92ad912dc681aac9c9fe3a89913cdd2
lulin-code <linlinlu77@163.com>
2021-10-29 13:58:32 +0800

    todolist消息订阅

--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
-/*
- * @Author: 卢林
- * @Date: 2021-10-25 14:24:00
- * @LastEditors: 卢林
- * @LastEditTime: 2021-10-28 17:09:38
- * @Descripttion: 文件描述
- */
-import Vue from 'vue';
+//引入Vue
+import Vue from 'vue'
+//引入App
 import App from './App.vue'
+//关闭Vue的生产提示
 Vue.config.productionTip = false
 
+//创建vm
 new Vue({
-  el:'#app',
-  render: h => h(App),
-  beforeCreate(){
-    Vue.prototype.$bus = this //安装全局事件总线
-  }
+	el:'#app',
+	render: h => h(App),
+	beforeCreate() {
+		Vue.prototype.$bus = this
+	},
 })
