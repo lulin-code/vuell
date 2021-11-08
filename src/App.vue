bb3282be0c88bb115118e56d933e0a420723596b
lulin-code <linlinlu77@163.com>
2021-11-08 11:29:07 +0800

    默认chacao

--- src/App.vue
+++ src/App.vue
@@ -1,121 +1,41 @@
 <template>
-	<div id="root">
-		<Search></Search>
-    <List></List>
-	</div>
+  <div id="root" class="container">
+    <Category title="美食">
+      <img src="https://s3.ax1x.com/2021/01/16/srJlq0.jpg" alt="" />
+    </Category>
+    <Category title="游戏">
+      <ul>
+        <li v-for="(g, index) in games" :key="index">{{ g }}</li>
+      </ul>
+    </Category>
+    <Category title="电影">
+      <video
+        controls
+        src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
+      ></video>
+    </Category>
+  </div>
 </template>
 
 <script>
-	import pubsub from 'pubsub-js'
-  import List from './components/List.vue'
-  import Search from './components/Search'
+import Category from './components/Category'
 
-	export default {
-		name:'App',
-		components:{Search,List},
-		data() {
-			return {
-				//由于todos是MyHeader组件和MyFooter组件都在使用，所以放在App中（状态提升）
-				todos:JSON.parse(localStorage.getItem('todos')) || []
-			}
-		},
-		methods: {
-			//添加一个todo
-			addTodo(todoObj){
-				this.todos.unshift(todoObj)
-			},
-			//勾选or取消勾选一个todo
-			checkTodo(id){
-				this.todos.forEach((todo)=>{
-					if(todo.id === id) todo.done = !todo.done
-				})
-			},
-			//更新一个todo
-			updateTodo(id,title){
-				this.todos.forEach((todo)=>{
-					if(todo.id === id) todo.title = title
-				})
-			},
-			//删除一个todo
-			deleteTodo(_,id){
-				this.todos = this.todos.filter( todo => todo.id !== id )
-			},
-			//全选or取消全选
-			checkAllTodo(done){
-				this.todos.forEach((todo)=>{
-					todo.done = done
-				})
-			},
-			//清除所有已经完成的todo
-			clearAllTodo(){
-				this.todos = this.todos.filter((todo)=>{
-					return !todo.done
-				})
-			}
-		},
-		watch: {
-			todos:{
-				deep:true,
-				handler(value){
-					localStorage.setItem('todos',JSON.stringify(value))
-				}
-			}
-		},
-		mounted() {
-			this.$bus.$on('checkTodo',this.checkTodo)
-			this.$bus.$on('updateTodo',this.updateTodo)
-			this.pubId = pubsub.subscribe('deleteTodo',this.deleteTodo)
-		},
-		beforeDestroy() {
-			this.$bus.$off('checkTodo')
-			this.$bus.$off('updateTodo')
-			pubsub.unsubscribe(this.pubId)
-		},
-	}
+export default {
+  name: 'App',
+  components: { Category },
+  data() {
+    return {
+      foods: ['火锅', '烧烤', '小龙虾', '牛排'],
+      games: ['红色警戒', '穿越火线', '劲舞团', '超级玛丽'],
+      films: ['《教父》', '《拆弹专家》', '《你好，李焕英》', '《尚硅谷》']
+    }
+  }
+}
 </script>
 
-<style>
-	/*base*/
-	body {
-		background: #fff;
-	}
-	.btn {
-		display: inline-block;
-		padding: 4px 12px;
-		margin-bottom: 0;
-		font-size: 14px;
-		line-height: 20px;
-		text-align: center;
-		vertical-align: middle;
-		cursor: pointer;
-		box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
-		border-radius: 4px;
-	}
-	.btn-danger {
-		color: #fff;
-		background-color: #da4f49;
-		border: 1px solid #bd362f;
-	}
-	.btn-edit {
-		color: #fff;
-		background-color: skyblue;
-		border: 1px solid rgb(103, 159, 180);
-		margin-right: 5px;
-	}
-	.btn-danger:hover {
-		color: #fff;
-		background-color: #bd362f;
-	}
-	.btn:focus {
-		outline: none;
-	}
-	.todo-container {
-		width: 600px;
-		margin: 0 auto;
-	}
-	.todo-container .todo-wrap {
-		padding: 10px;
-		border: 1px solid #ddd;
-		border-radius: 5px;
+<style scoped>
+.container{
+		display: flex;
+		justify-content: space-around;
 	}
 </style>
