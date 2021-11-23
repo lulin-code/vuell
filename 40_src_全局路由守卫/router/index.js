9fc098e237350492bcf4a2afa8da1ed77976cc39
lulin-code <linlinlu77@163.com>
2021-11-23 16:06:35 +0800

    全局路由守卫

--- 40_src_全局路由守卫/router/index.js
+++ 40_src_全局路由守卫/router/index.js
@@ -8,38 +8,38 @@ import Message from '../pages/Message'
 import Detail from '../pages/Detail'
 
 //创建并暴露一个路由器
-const router = new VueRouter({
+const router =  new VueRouter({
 	routes:[
 		{
 			name:'guanyu',
 			path:'/about',
 			component:About,
-      meta:{title:'关于'}
+			meta:{title:'关于'}
 		},
 		{
-      name: 'zhuye',
+			name:'zhuye',
 			path:'/home',
 			component:Home,
-      meta: { title: '首页' },
+			meta:{title:'主页'},
 			children:[
 				{
-          name: 'xinwen',
+					name:'xinwen',
 					path:'news',
 					component:News,
-          meta: { isAuth: true, title: '新闻' }
+					meta:{isAuth:true,title:'新闻'}
 				},
 				{
-          name:'xiaoxi',
+					name:'xiaoxi',
 					path:'message',
 					component:Message,
-          meta: { isAuth: true, title: '消息' },
+					meta:{isAuth:true,title:'消息'},
 					children:[
 						{
 							name:'xiangqing',
 							path:'detail',
 							component:Detail,
-              meta: { isAuth: true, title: '详情' },
-              
+							meta:{isAuth:true,title:'详情'},
+
 							//props的第一种写法，值为对象，该对象中的所有key-value都会以props的形式传给Detail组件。
 							// props:{a:1,b:'hello'}
 
@@ -64,21 +64,24 @@ const router = new VueRouter({
 	]
 })
 
-router.beforeEach((to,from,next)=> {
-  console.log('前置路由守卫',to,from)
-  if (to.meta.isAuth) {
-    if (localStorage.getItem('school') === 'll') {
-      next()
-    } else {
-      alert('学校名不对，无权限查看')
-    }
-  } else {
-    next()
-  }
+//全局前置路由守卫————初始化的时候被调用、每次路由切换之前被调用
+router.beforeEach((to,from,next)=>{
+	console.log('前置路由守卫',to,from)
+	if(to.meta.isAuth){ //判断是否需要鉴权
+		if(localStorage.getItem('school')==='atguigu'){
+			next()
+		}else{
+			alert('学校名不对，无权限查看！')
+		}
+	}else{
+		next()
+	}
 })
 
-router.afterEach((to,from)=> {
-  console.log('后置路由守卫',to,from)
-  document.title = to.meta.title || '硅谷系统'
+//全局后置路由守卫————初始化的时候被调用、每次路由切换之后被调用
+router.afterEach((to,from)=>{
+	console.log('后置路由守卫',to,from)
+	document.title = to.meta.title || '硅谷系统'
 })
+
 export default router
