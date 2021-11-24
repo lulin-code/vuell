cea5d24632338622867b85189738fd4f52cf436f
lulin-code <linlinlu77@163.com>
2021-11-24 15:17:50 +0800

    组件内路由守卫

--- 42_src_组件内路由守卫/router/index.js
+++ 42_src_组件内路由守卫/router/index.js
@@ -8,50 +8,50 @@ import Message from '../pages/Message'
 import Detail from '../pages/Detail'
 
 //创建并暴露一个路由器
-const router = new VueRouter({
+const router =  new VueRouter({
 	routes:[
 		{
 			name:'guanyu',
 			path:'/about',
 			component:About,
-      meta: { isAuth: true,title:'关于'}
+			meta:{isAuth:true,title:'关于'}
 		},
 		{
-      name: 'zhuye',
+			name:'zhuye',
 			path:'/home',
 			component:Home,
-      meta: { title: '首页' },
+			meta:{title:'主页'},
 			children:[
 				{
-          name: 'xinwen',
+					name:'xinwen',
 					path:'news',
 					component:News,
-          meta: { isAuth: true, title: '新闻' },
-          beforeEnter: (to, from, next) => {
-            // 判断是否需要鉴权
-            if(to.meta.isAuth){
-              if (localStorage.getItem('school') === 'll') {
-                next()
-              } else {
-                alert('学校名不对，无权限查看')
-              }
-            }else {
-              next()
-            }
-          }
+					meta:{isAuth:true,title:'新闻'},
+					/* beforeEnter: (to, from, next) => {
+						console.log('前置路由守卫',to,from)
+						if(to.meta.isAuth){ //判断是否需要鉴权
+							if(localStorage.getItem('school')==='atguigu'){
+								next()
+							}else{
+								alert('学校名不对，无权限查看！')
+							}
+						}else{
+							next()
+						}
+					} */
 				},
 				{
-          name:'xiaoxi',
+					name:'xiaoxi',
 					path:'message',
 					component:Message,
-          meta: { isAuth: true, title: '消息' },
+					meta:{isAuth:true,title:'消息'},
 					children:[
 						{
 							name:'xiangqing',
 							path:'detail',
 							component:Detail,
-              meta: { isAuth: true, title: '详情' },
-              
+							meta:{isAuth:true,title:'详情'},
+
 							//props的第一种写法，值为对象，该对象中的所有key-value都会以props的形式传给Detail组件。
 							// props:{a:1,b:'hello'}
 
@@ -76,10 +76,24 @@ const router = new VueRouter({
 	]
 })
 
+//全局前置路由守卫————初始化的时候被调用、每次路由切换之前被调用
+/* router.beforeEach((to,from,next)=>{
+	console.log('前置路由守卫',to,from)
+	if(to.meta.isAuth){ //判断是否需要鉴权
+		if(localStorage.getItem('school')==='atguigu'){
+			next()
+		}else{
+			alert('学校名不对，无权限查看！')
+		}
+	}else{
+		next()
+	}
+}) */
 
-
-router.afterEach((to,from)=> {
-  console.log('后置路由守卫',to,from)
-  document.title = to.meta.title || '硅谷系统'
+//全局后置路由守卫————初始化的时候被调用、每次路由切换之后被调用
+router.afterEach((to,from)=>{
+	console.log('后置路由守卫',to,from)
+	document.title = to.meta.title || '硅谷系统'
 })
+
 export default router
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Author: 卢林
+ * @Date: 2021-11-22 18:31:02
+ * @LastEditors: 卢林
+ * @LastEditTime: 2021-11-24 14:35:16
+ * @Descripttion: 文件描述
+-->
 <template>
 	<h2>我是About的内容</h2>
 </template>
@@ -5,13 +12,21 @@
 <script>
 	export default {
 		name:'About',
-		/* beforeDestroy() {
-			console.log('About组件即将被销毁了')
-		},*/
-		/* mounted() {
-			console.log('About组件挂载完毕了',this)
-			window.aboutRoute = this.$route
-			window.aboutRouter = this.$router
-		},  */
+    beforeRouteEnter (to, from, next) {
+      console.log('About---beforeRouterEnter',to,from)
+      if(to.meta.isAuth){
+        if(localStorage.getItem('school') === 'll'){
+          next()
+        }else{
+          alert('学校名不对，无权查看')
+        }
+      }else{
+        next()
+      }
+    },
+    beforeRouteLeave (to, from, next) {
+      console.log('About---beforeRouteLeave',to,from)
+      next()
+    }
 	}
 </script>
